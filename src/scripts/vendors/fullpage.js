eb8987d3f6bd0b9bb37c005fdc693a016578787a
AdminingSS <admining@mail.ru>
2019-06-12 21:19:26 +0300

    !!!2

--- src/scripts/vendors/fullpage.js
+++ src/scripts/vendors/fullpage.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         normalScrollElements: '.tm-scrollable',
         verticalCentered: false,
-        //fitToSection: false,
+        fitToSection: false,
         paddingTop: '120px',
         fixedElements: 'header, aside',
         scrollOverflow: true,
+        scrollOverflowReset: true,
+        menu: '#controls',
+        normalScrollElementTouchThreshold: 3
 
         //options here
         //autoScrolling:false
         //scrollHorizontally: true
     });
 
-    //$('.tm-scrollable').attr('style','width: 101%;')
-
-    //methods
     //$.fn.fullpage.setAllowScrolling(false);
 });
